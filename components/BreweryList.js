0e2816e973139045743ff01ba8e95eb8d01fc6fc
Brent Vatne <brentvatne@gmail.com>
2016-09-22 10:25:34 -0700

    Step 2 - Move BreweryList out into its own component

---
+++ components/BreweryList.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {
+  Location,
+  Permissions,
+} from 'exponent';
+import {
+  Platform,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import geolib from 'geolib';
+
+import BreweryListItem from './BreweryListItem';
+import breweries from '../data';
+
+
+export default class BreweryList extends React.Component {
+  state = {
+    breweries,
+  }
+
+  componentWillMount() {
+    this._computeDistancesAsync();
+  }
+
+  _computeDistancesAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') { return; }
+
+    let { coords } = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: Platform.OS === 'ios',
+    });
+
+    let breweriesWithDistances = this.state.breweries.map(brewery => {
+      let distanceM = geolib.getDistance(
+        {latitude: coords.latitude, longitude: coords.longitude},
+        {latitude: brewery.latitude, longitude: brewery.longitude},
+      )
+
+      let distanceKm = (distanceM / 1000.0).toFixed(2);
+      let formattedDistance = `${distanceKm}km`;
+
+      let direction = geolib.getCompassDirection(
+        {latitude: coords.latitude, longitude: coords.longitude},
+        {latitude: brewery.latitude, longitude: brewery.longitude},
+      );
+
+      return {
+        ...brewery,
+        distance: formattedDistance,
+        direction: direction,
+      };
+    });
+
+    this.setState({
+      breweries: breweriesWithDistances,
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        {
+          this.state.breweries.map(brewery => (
+            <BreweryListItem
+              brewery={brewery}
+              key={brewery.name}
+            />
+          ))
+        }
+
+        <StatusBar barStyle="default" />
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FBFBFB',
+  },
+});
--- main.js
+++ main.js
@@ -1,32 +1,24 @@
 import Exponent, {
   Font,
-  Location,
-  Permissions,
 } from 'exponent';
 import React from 'react';
 import {
-  Platform,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
 import { MaterialIcons } from '@exponent/vector-icons';
-import geolib from 'geolib';
 
-import BreweryListItem from './components/BreweryListItem';
-import breweries from './data';
+import BreweryList from './components/BreweryList';
 
 class App extends React.Component {
   state = {
     appIsReady: false,
-    breweries,
   };
 
   componentWillMount() {
     this._loadAssetsAsync();
-    this._computeDistancesAsync();
   }
 
   _loadAssetsAsync = async () => {
@@ -42,66 +34,18 @@ class App extends React.Component {
     });
   }
 
-  _computeDistancesAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') { return; }
-
-    let { coords } = await Location.getCurrentPositionAsync({
-      enableHighAccuracy: Platform.OS === 'ios',
-    });
-
-    let breweriesWithDistances = this.state.breweries.map(brewery => {
-      let distanceM = geolib.getDistance(
-        {latitude: coords.latitude, longitude: coords.longitude},
-        {latitude: brewery.latitude, longitude: brewery.longitude},
-      )
-
-      let distanceKm = (distanceM / 1000.0).toFixed(2);
-      let formattedDistance = `${distanceKm}km`;
-
-      let direction = geolib.getCompassDirection(
-        {latitude: coords.latitude, longitude: coords.longitude},
-        {latitude: brewery.latitude, longitude: brewery.longitude},
-      );
-
-      return {
-        ...brewery,
-        distance: formattedDistance,
-        direction: direction,
-      };
-    });
-
-    this.setState({
-      breweries: breweriesWithDistances,
-    });
-  }
-
   render() {
     if (!this.state.appIsReady) {
       return <Exponent.Components.AppLoading />;
     }
 
-    return (
-      <ScrollView style={styles.container}>
-        {
-          this.state.breweries.map(brewery => (
-            <BreweryListItem
-              brewery={brewery}
-              key={brewery.name}
-            />
-          ))
-        }
-
-        <StatusBar barStyle="default" />
-      </ScrollView>
-    );
+    return <BreweryList />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FBFBFB',
   },
 });
 
