7ec0d6fbd648a884ea30808d57da33a86a05c231
Brent Vatne <brentvatne@gmail.com>
2017-01-28 15:52:46 -0800

    Remove show/hide navigation bar anim

--- screens/BreweryListScreen.js
+++ screens/BreweryListScreen.js
@@ -36,9 +36,6 @@ const MenuOptions = [
   'Unvisited'
 ];
 
-const maxOffset = NavigationBar.DEFAULT_HEIGHT - Constants.statusBarHeight;
-const minScrollViewHeight = 100;
-
 @connect()
 export default class BreweryListScreen extends React.Component {
   static route = {
@@ -49,84 +46,12 @@ export default class BreweryListScreen extends React.Component {
 
   state = {
     menuIsVisible: false,
-    contentHeight: Layout.window.height,
-    maxScrollY: minScrollViewHeight,
-    scrollViewHeight: Layout.window.height - minScrollViewHeight,
     menuValue: new Animated.Value(0),
-    scrollY: new Animated.Value(0),
     selectedOption: MenuOptions[0],
   }
 
-  _initialScrollYForTouch: 0;
-  _lastToValue = 0;
-  _scrollY = 0;
-  _hasScroll = false;
-  _hasMomentum = false;
-  _scrollEndTimer: any;
-
   componentDidMount() {
     this.props.dispatch(Actions.computeDistances());
-
-    this.state.scrollY.addListener(({value}) => {
-      this._scrollY = Math.min(Math.max(0, value), this.state.maxScrollY);
-    });
-  }
-
-  _onMomentumScrollBegin = () => {
-    this._hasMomentum = true;
-  }
-
-  _onMomentumScrollEnd = () => {
-    clearTimeout(this._scrollEndTimer);
-    this._hasMomentum = false;
-    this._onScrollEnd();
-  };
-
-  _onScrollBeginDrag = () => {
-    this._initialScrollYForTouch = this._scrollY;
-  }
-
-  _onScrollEndDrag = () => {
-    // `onMomentumScrollEnd` is not always called so wait a little bit to check
-    // if there is momentum scrolling and if there is adjust navbar in
-    // `onMomentumScrollEnd` instead.
-    clearTimeout(this._scrollEndTimer);
-    this._scrollEndTimer = setTimeout(() => {
-      if (!this._hasMomentum) {
-        this._onScrollEnd();
-      }
-    }, 100);
-  };
-
-  _onScrollEnd = () => {
-    let deltaY = this._scrollY - this._initialScrollYForTouch;
-
-    if (deltaY > maxOffset || deltaY < -maxOffset) {
-      return;
-    }
-
-    if (this._scrollY <= 0 || this._scrollY >= this.state.maxScrollY) {
-      return;
-    }
-
-    if (deltaY > 0) {
-      this._list.scrollTo({
-        y: this._scrollY + maxOffset - deltaY,
-        x: 0,
-        animated: true,
-      });
-    } else {
-      this._list.scrollTo({
-        y: this._scrollY + (-maxOffset - deltaY),
-        x: 0,
-        animated: true,
-      });
-    }
-  };
-
-  _updateContentHeight = (w, h) => {
-    const maxScrollY = Math.max(h - this.state.scrollViewHeight + maxOffset, 101);
-    this.setState({contentHeight: h, maxScrollY});
   }
 
   render() {
@@ -135,16 +60,6 @@ export default class BreweryListScreen extends React.Component {
       key: 'list',
       setRef: view => { this._list = view; },
       contentContainerStyle: {marginTop: NavigationBar.DEFAULT_HEIGHT},
-      onContentSizeChange: this._updateContentHeight,
-      onLayout: ({nativeEvent}) => { this.setState({scrollViewHeight: nativeEvent.layout.height}) },
-      onMomentumScrollBegin: this._onMomentScrollBegin,
-      onMomentumScrollEnd: this._onMomentScrollEnd,
-      onScrollBeginDrag: this._onScrollBeginDrag,
-      onScrollEndDrag: this._onScrollEndDrag,
-      onScroll: Animated.event(
-        [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
-        { useNativeDriver: false }
-      )
     };
 
     return (
@@ -209,90 +124,22 @@ export default class BreweryListScreen extends React.Component {
 
   _navigationBarAnimatedStyles = {};
 
-  _getNavigationBarAnimatedStyles = (maxScrollY) => {
-    if (this._navigationBarAnimatedStyles.maxScrollY !== maxScrollY ||
-        this.state.selectedOption !== this._navigationBarAnimatedStyles.selectedOption) {
-      this._navigationBarAnimatedStyles = {};
-    } else if (Object.keys(this._navigationBarAnimatedStyles).length) {
-      return this._navigationBarAnimatedStyles;
-    }
-
-    let {
-      scrollY,
-    } = this.state;
-
-    let invertedClampedScrollY = scrollY.interpolate({
-      inputRange: [0, maxScrollY],
-      outputRange: [0, -maxScrollY],
-      extrapolate: 'clamp',
-    });
-
-    let translateY = Animated.diffClamp(
-      invertedClampedScrollY,
-      -maxOffset,
-      0,
-    );
-
-    let opacity = translateY.interpolate({
-      inputRange: [-maxOffset, -maxOffset / 2, 0],
-      outputRange: [0, 0, 1],
-    });
-
-    let scale = translateY.interpolate({
-      inputRange: [-maxOffset, 0],
-      outputRange: [Platform.OS === 'ios' ? 0 : 1, 1],
-    });
-
-    let contentTranslateY = translateY.interpolate({
-      inputRange: [-maxOffset, 0],
-      outputRange: [Platform.OS === 'ios' ? maxOffset / 1.5 : 0, 0],
-    });
-
-    this._navigationBarAnimatedStyles = {
-      maxScrollY,
-      translateY,
-      opacity,
-      scale,
-      contentTranslateY,
-      selectedOption: this.state.selectedOption,
-    };
-
-    return this._navigationBarAnimatedStyles;
-  }
-
   _renderNavigationBar() {
     let {
       contentHeight,
-      maxScrollY,
       menuValue,
-      navBarShowAnim,
-      scrollViewHeight,
     } = this.state;
 
     let arrowRotation = menuValue.interpolate({
       inputRange: [0, 1],
       outputRange: ['90deg', '-90deg'],
     });
-
-    let {
-      translateY,
-      opacity,
-      scale,
-      contentTranslateY,
-    } = this._getNavigationBarAnimatedStyles(maxScrollY);
-
     return (
-      <Animated.View
-        key="navbar"
-        style={[
-          styles.navigationBarContainer,
-          {overflow: 'hidden', paddingTop: Constants.statusBarHeight, transform: [{translateY}]}
-        ]}>
-        <Animated.View
-          style={[
-            styles.navigationBarTitleContainer,
-            {opacity, transform: [{scale}, {translateY: contentTranslateY}]}
-          ]}>
+      <View key="navbar" style={[
+        styles.navigationBarContainer,
+        {overflow: 'hidden', paddingTop: Constants.statusBarHeight}
+      ]}>
+        <View style={styles.navigationBarTitleContainer}>
           <TouchableWithoutFeedback
             hitSlop={{left: 40, top: 30, right: 40, bottom: 10}}
             onPress={this._handleToggleMenu}>
@@ -306,18 +153,14 @@ export default class BreweryListScreen extends React.Component {
               </Animated.View>
             </View>
           </TouchableWithoutFeedback>
-        </Animated.View>
+        </View>
 
-        <Animated.View
-          style={[
-            styles.navigationBarRightButton,
-            {opacity, transform: [{scale}, {translateY: contentTranslateY}]}
-          ]}>
+        <View style={styles.navigationBarRightButton}>
           <TouchableNativeFeedback onPress={this._handlePressUpdateLocation}>
             <MaterialIcons name="my-location" size={20} />
           </TouchableNativeFeedback>
-        </Animated.View>
-      </Animated.View>
+        </View>
+      </View>
     );
   }
 
@@ -327,20 +170,6 @@ export default class BreweryListScreen extends React.Component {
   }
 
   _handleToggleMenu = () => {
-    if (this._hasMomentum) {
-      return;
-    }
-
-    let { translateY } = this._getNavigationBarAnimatedStyles(this.state.maxScrollY);
-    let currentTranslateY = translateY.__getValue();
-    if (currentTranslateY < 0) {
-      this._list.scrollTo({
-        y: this._scrollY - (maxOffset + currentTranslateY),
-        x: 0,
-        animated: true,
-      });
-    }
-
     let { menuIsVisible } = this.state;
     let onCompleteAnimation = () => {}
 
